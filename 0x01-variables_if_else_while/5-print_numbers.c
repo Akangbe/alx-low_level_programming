9baa464d8fef40297ce9ef238051f53b05b92bb9
Akangbe <akangbehenry28@gmail.com>
2023-03-20 16:59:09 -0700

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -17,25 +17,3 @@ int main(void)
 	return (0);
 }
 
-
-6-print_numberz.c
-
-#include <stdio.h>
-
-/**
- * main - Prints numbers from 0 to 9.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
-}
-
